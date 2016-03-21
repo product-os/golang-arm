3373c6893a0cbe1fd02e166cde0d0acffa3c7181
James <james@resin.io>
2016-03-21 20:48:20 +0700

    Add checksums verification for go-bootstrap binaries.

--- build.sh
+++ build.sh
@@ -9,12 +9,6 @@ GOROOT_BOOTSTRAP_VERSION=1.4.3
 TAR_FILE=go$GOLANG_VERSION.linux-$ARCH.tar.gz
 BUCKET_NAME=$BUCKET_NAME
 
-COMMIT_1_4_3='50eb39bb23e8b03e823c38e844f0410d0b5325d2'
-COMMIT_1_5_1='f2e4c8b5fb3660d793b2c545ef207153db0a34b1'
-COMMIT_1_5_2='40cbf58f960a8f5287d2c3a93b3ca6119df67e85'
-COMMIT_1_5_3='27d5c0ede5b4411089f4bf52a41dd2f4eed36123'
-COMMIT_1_6='7bc40ffb05d8813bf9b41a331b45d37216f9e747'
-
 # comparing version: http://stackoverflow.com/questions/16989598/bash-comparing-version-numbers
 function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" == "$1"; }
 
@@ -22,6 +16,7 @@ function version_le() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" 
 if version_le $GOLANG_VERSION "1.5"; then
 	mkdir /go-bootstrap
 	wget http://resin-packages.s3.amazonaws.com/golang/v$GOROOT_BOOTSTRAP_VERSION/go$GOROOT_BOOTSTRAP_VERSION.linux-$ARCH.tar.gz
+	echo "$(grep " go$GOROOT_BOOTSTRAP_VERSION.linux-$ARCH.tar.gz" /checksums-commit-table)" | sha256sum -c -
 	tar -xzf "go$GOROOT_BOOTSTRAP_VERSION.linux-$ARCH.tar.gz" -C /go-bootstrap --strip-components=1
 	rm go$GOROOT_BOOTSTRAP_VERSION.linux-$ARCH.tar.gz
 	export GOROOT_BOOTSTRAP=/go-bootstrap
@@ -50,8 +45,8 @@ esac
 echo "GOARM: $GOARM"
 echo "GOARCH: $GOARCH"
 
-COMMIT=COMMIT_${GOLANG_VERSION//./_}
-cd go && git checkout $(eval echo \$$COMMIT)
+commit=($(echo "$(grep " go$GOLANG_VERSION" /checksums-commit-table)" | tr " " "\n"))
+cd go && git checkout ${commit[0]}
 
 # There is an issue with musl libc and Go v1.6 on Alpine i386 image (https://github.com/golang/go/issues/14476)
 # So we need to patch Go (https://github.com/golang/go/commit/1439158120742e5f41825de90a76b680da64bf76)
