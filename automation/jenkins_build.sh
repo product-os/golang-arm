8243afabc91f74e1d5ae2ca58e0ddb9214c1c8a4
Trong Nghia Nguyen <nghiant2710@gmail.com>
2022-01-11 12:56:52 +0700

    Update base version for Debian and Alpine Linux base image
    Update to Debian Buster and Alpine Linux 3.13
    
    Change-type: patch
    Signed-off-by: Trong Nghia Nguyen <nghiant2710@gmail.com>

--- automation/jenkins_build.sh
+++ automation/jenkins_build.sh
@@ -6,48 +6,48 @@ for ARCH in $ARCHS
 do
 	case "$ARCH" in
 		'armv6hf')
-			sed -e s~#{FROM}~balenalib/rpi-raspbian:jessie~g \
+			sed -e s~#{FROM}~balenalib/rpi-raspbian:buster~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-arm-static /usr/bin/"~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'armv7hf')
-			sed -e s~#{FROM}~balenalib/armv7hf-debian:jessie~g \
+			sed -e s~#{FROM}~balenalib/armv7hf-debian:buster~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-arm-static /usr/bin/"~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'armel')
-			sed -e s~#{FROM}~balenalib/armel-debian:jessie~g \
+			sed -e s~#{FROM}~balenalib/armel-debian:buster~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-arm-static /usr/bin/"~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'aarch64')
-			sed -e s~#{FROM}~balenalib/aarch64-debian:jessie~g \
+			sed -e s~#{FROM}~balenalib/aarch64-debian:buster~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-aarch64-static /usr/bin/"~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'i386')
-			sed -e s~#{FROM}~balenalib/i386-debian:jessie~g \
+			sed -e s~#{FROM}~balenalib/i386-debian:buster~g \
 				-e s~#{QEMU}~""~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'i386-nlp')
-			sed -e s~#{FROM}~balenalib/i386-nlp-debian:jessie~g \
+			sed -e s~#{FROM}~balenalib/i386-nlp-debian:buster~g \
 				-e s~#{QEMU}~""~g Dockerfile.debian.tpl > Dockerfile
 		;;
 		'alpine-armv6hf')
-			sed -e s~#{FROM}~balenalib/rpi-alpine:3.9~g \
+			sed -e s~#{FROM}~balenalib/rpi-alpine:3.13~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-arm-static /usr/bin/"~g Dockerfile.alpine.tpl > Dockerfile
 		;;
 		'alpine-i386')
-			sed -e s~#{FROM}~balenalib/i386-alpine:3.9~g \
+			sed -e s~#{FROM}~balenalib/i386-alpine:3.13~g \
 				-e s~#{QEMU}~""~g Dockerfile.alpine.tpl > Dockerfile
 		;;
 		'alpine-amd64')
-			sed -e s~#{FROM}~balenalib/amd64-alpine:3.9~g \
+			sed -e s~#{FROM}~balenalib/amd64-alpine:3.13~g \
 				-e s~#{QEMU}~""~g Dockerfile.alpine.tpl > Dockerfile
 		;;
 		'alpine-aarch64')
-			sed -e s~#{FROM}~balenalib/aarch64-alpine:3.9~g \
+			sed -e s~#{FROM}~balenalib/aarch64-alpine:3.13~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-aarch64-static /usr/bin/"~g Dockerfile.alpine.tpl > Dockerfile
 		;;
 		'alpine-armv7hf')
 			# armv7hf-alpine v3.9 and later are armv7
-			sed -e s~#{FROM}~balenalib/armv7hf-alpine:3.9~g \
+			sed -e s~#{FROM}~balenalib/armv7hf-alpine:3.13~g \
 				-e s~#{QEMU}~"COPY qemu/qemu-arm-static /usr/bin/"~g Dockerfile.alpine.tpl > Dockerfile
 		;;
 	esac
